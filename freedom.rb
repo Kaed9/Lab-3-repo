ed851a6080b2f4d69eb8f946d8f7e6886084e96b
Kenneth <kaelmitsuhiko11@gmail.com>
2018-02-27 14:52:15 +0800

    Submit button update yehey

--- freedom.rb
+++ freedom.rb
@@ -1,6 +1,14 @@
 require 'sinatra'
 require 'yaml/store'
 
-get '/' do
-	erb :index
-end
+# class FreedomBoard < Sinatra::Base
+	get '/' do
+		erb :index
+	end
+
+	post '/cast' do
+		store = YAML::Store.new "messages.store"
+		
+		erb :cast
+	end
+# end
