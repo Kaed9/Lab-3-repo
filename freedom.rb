59d37c23e8eadd92a689877a84365d6e47697f3a
Kenneth <kaelmitsuhiko11@gmail.com>
2018-02-28 16:32:10 +0800

    config.ru update 2

--- freedom.rb
+++ freedom.rb
@@ -1,28 +1,30 @@
 require 'sinatra'
 require 'yaml/store'
 
-get '/' do
-	erb :index
-end
-
-post '/cast' do
-	@store = YAML::Store.new "messages.yml"
-	@name = params['name_area']
-	@message = params['message_area']
-	@date = DateTime.now
-	@date.strftime("%m/%d/%Y,%I:%M:%L")
-	@store.transaction do
-		@store[@date] ||= {@name => @message}
+class FreedomBoard < Sinatra::Base
+	get '/' do
+		erb :index
 	end
-	erb :index
-end
 
-post '/search' do
-	@search_item = params['search_bar']
-	@counter = 0
-	erb :search
-end
+	post '/cast' do
+		@store = YAML::Store.new "messages.yml"
+		@name = params['name_area']
+		@message = params['message_area']
+		@date = DateTime.now
+		@date.strftime("%m/%d/%Y,%I:%M:%L")
+		@store.transaction do
+			@store[@date] ||= {@name => @message}
+		end
+		erb :index
+	end
 
-post '/return' do
-	erb :index
+	post '/search' do
+		@search_item = params['search_bar']
+		@counter = 0
+		erb :search
+	end
+
+	post '/return' do
+		erb :index
+	end
 end
